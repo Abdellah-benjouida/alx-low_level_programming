e9bcae28948a43b692337b37f8ce6dba76214b42
abdellah-benjouida <abdellahbenjouida67@gmail.com>
2023-06-20 19:57:11 -0700

    3d task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,26 +1,29 @@
 #include "main.h"
 
 
-/**
- * print_alphabet_x10 - make alphabet x10 times
- * return: void
- */
+	/**
+	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+	 * followed by a new line
+	 */
+	void print_alphabet_x10(void)
+	{
+		char ch;
+		int i;
 
-void print_alphabet_x10(void)
-{
-	char ch;
-	int i;
-	i = 0;
-	while (i < 10)
-	{
-	ch = 'a';
-	while (ch <= 'z')
-	{
-		_putchar(ch);
-		ch++;
+
+		i = 0;
+
+
+		while (i < 10)
+		{
+			ch = 'a';
+			while (ch <= 'z')
+			{
+				_putchar(ch);
+				ch++;
+			}
+			_putchar('\n');
+			i++;
+		}
 	}
-	_putchar('\n');
-	i++;
-	}
-}
 
